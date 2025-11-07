203e7d48a558ca566ba7d1434aa8bceacda635c3
raaakib0 <raaakib0@gmail.com>
2025-11-07 21:51:20 +0600

    dark modd icon toggle

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ function toggleMenu() {
 
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
+
+  // Change icon
+  const icon = document.getElementById('dark-icon');
+  if (document.body.classList.contains('dark-mode')) {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  } else {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  }
 }
+
